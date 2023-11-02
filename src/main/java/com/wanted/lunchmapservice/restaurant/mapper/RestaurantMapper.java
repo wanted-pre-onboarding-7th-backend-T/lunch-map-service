925f48838dd8cb4ce96036eb66d4fa5cfd5b9fd3
Siglee2247 <lgh941107@gmail.com>
2023-11-02 21:42:51 +0900

    feat: RestaurantService getDetails 메서드 구현 #1 - 구현을 위한 RestaurantRepository 구현 - 데이터 변환을 위한 RestaurantMapper 구현

---
+++ src/main/java/com/wanted/lunchmapservice/restaurant/mapper/RestaurantMapper.java
@@ -0,0 +1,43 @@
+package com.wanted.lunchmapservice.restaurant.mapper;
+
+import com.wanted.lunchmapservice.common.dto.ResponseDto;
+import com.wanted.lunchmapservice.location.dto.LocationDto;
+import com.wanted.lunchmapservice.location.entity.Location;
+import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestaurantMapper {
+
+    public ResponseDto<GetRestaurantDetailResponseDto> toResponseDto(Restaurant entity) {
+        return ResponseDto.<GetRestaurantDetailResponseDto>builder()
+                .data(toDto(entity))
+                .message(HttpStatus.OK.getReasonPhrase())
+                .code(HttpStatus.OK.value())
+                .build();
+    }
+
+    private GetRestaurantDetailResponseDto toDto(Restaurant entity) {
+        return GetRestaurantDetailResponseDto.builder()
+                .restaurantId(entity.getId())
+                .averageScore(entity.getAverageScore())
+                .restaurantName(entity.getName())
+                .lotNumberAddress(entity.getLotNumberAddress())
+                .roadNameAddress(entity.getRoadNameAddress())
+                .location(toDto(entity.getLocation()))
+                .zipCode(entity.getZipCode())
+                .build();
+    }
+
+    private LocationDto toDto(Location entity) {
+        return LocationDto.builder()
+                .cityName(entity.getCityName())
+                .countryName(entity.getCountryName())
+                .longitude(entity.getLongitude())
+                .latitude(entity.getLatitude())
+                .build();
+    }
+
+}
---
+++ src/main/java/com/wanted/lunchmapservice/restaurant/service/RestaurantGetService.java
@@ -0,0 +1,28 @@
+package com.wanted.lunchmapservice.restaurant.service;
+
+import com.wanted.lunchmapservice.common.dto.ResponseDto;
+import com.wanted.lunchmapservice.common.exception.CommonException;
+import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
+import com.wanted.lunchmapservice.restaurant.mapper.RestaurantMapper;
+import com.wanted.lunchmapservice.restaurant.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RestaurantGetService {
+    private final RestaurantRepository repository;
+    private final RestaurantMapper mapper;
+    public ResponseDto<GetRestaurantDetailResponseDto> getDetails(Long restaurantId) {
+        return mapper.toResponseDto(validRestaurant(restaurantId));
+    }
+
+    private Restaurant validRestaurant(Long restaurantId) {
+        return repository.findById(restaurantId)
+                .orElseThrow(() -> new CommonException(HttpStatus.CONFLICT, "맛집 정보가 존재하지 않습니다."));
+    }
+}
