3beea7b6aeaed3628501dc31a2da6fb9458be458
Daeun Kim <jkde7721@naver.com>
2023-11-02 18:46:47 +0900

    update: User 엔티티 필드명 변경

--- src/main/java/com/wanted/lunchmapservice/user/entity/User.java
+++ src/main/java/com/wanted/lunchmapservice/user/entity/User.java
@@ -33,15 +33,14 @@ public class User extends BaseTime {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "lat")
-    private Double lat;
+    @Column(name = "latitude")
+    private Double latitude;
 
-    @Column(name = "lon")
-    private Double lon;
+    @Column(name = "longitude")
+    private Double longitude;
 
     @Enumerated(EnumType.STRING)
     @ColumnDefault("'NONE'")
     @Column(name = "service_access")
     private ServiceAccess serviceAccess;
-
 }
