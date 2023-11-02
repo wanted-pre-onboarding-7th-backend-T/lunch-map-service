ed48260ccd9f864c33eaddff511b9d5e35c7c2a8
Siglee2247 <lgh941107@gmail.com>
2023-11-03 02:05:22 +0900

    update: getter Lombok 변환 #1

--- src/test/java/com/wanted/lunchmapservice/restaurant/mock/RestaurantMock.java
+++ src/test/java/com/wanted/lunchmapservice/restaurant/mock/RestaurantMock.java
@@ -5,21 +5,23 @@ import com.wanted.lunchmapservice.location.dto.LocationDto;
 import com.wanted.lunchmapservice.location.entity.Location;
 import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class RestaurantMock {
 
-    private String name = "아도니스";
-    private String lotNumberAddress = "경기도 가평군 상면 행현리 602-3번지";
-    private String roadNameAddress = "경기도 가평군 상면 수목원로 314-2";
-    private String zipCode = "12448";
-    private Double longitude = 37.7516678333;
-    private Double latitude = 127.3588076752;
-    private Double averageScore = 0.0;
-    private String cityName = "경기도";
-    private String countryName = "가평군";
+    private final String name = "아도니스";
+    private final String lotNumberAddress = "경기도 가평군 상면 행현리 602-3번지";
+    private final String roadNameAddress = "경기도 가평군 상면 수목원로 314-2";
+    private final String zipCode = "12448";
+    private final Double longitude = 37.7516678333;
+    private final Double latitude = 127.3588076752;
+    private final Double averageScore = 0.0;
+    private final String cityName = "경기도";
+    private final String countryName = "가평군";
 
     public Restaurant getEntity(Long id) {
         return Restaurant.builder()
@@ -76,41 +78,6 @@ public class RestaurantMock {
                 .build();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getLotNumberAddress() {
-        return lotNumberAddress;
-    }
-
-    public String getRoadNameAddress() {
-        return roadNameAddress;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public Double getAverageScore() {
-        return averageScore;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
 }
 
 
