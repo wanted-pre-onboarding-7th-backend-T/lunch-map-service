6239be1eadc2677e1cd38b04817d303f0ebe6071
Siglee2247 <lgh941107@gmail.com>
2023-11-03 10:35:47 +0900

    fit: Restaurant getRestaurant 메서드 구현을 위한 mapper 구현 #1

--- src/main/java/com/wanted/lunchmapservice/restaurant/mapper/RestaurantMapper.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/mapper/RestaurantMapper.java
@@ -1,13 +1,17 @@
 package com.wanted.lunchmapservice.restaurant.mapper;
 
+import com.wanted.lunchmapservice.common.dto.CustomPage;
+import com.wanted.lunchmapservice.common.dto.PageInfo;
 import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.location.dto.LocationDto;
 import com.wanted.lunchmapservice.location.entity.Location;
 import com.wanted.lunchmapservice.rating.Rating;
 import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantSimpleResponseDto;
 import com.wanted.lunchmapservice.restaurant.dto.RatingDto;
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +23,40 @@ public class RestaurantMapper {
                 .data(toDto(entity))
                 .message(HttpStatus.OK.getReasonPhrase())
                 .code(HttpStatus.OK.value())
+                .build();
+    }
+
+    public ResponseDto<CustomPage<GetRestaurantSimpleResponseDto>> toResponseDto(Page<Restaurant> entityPage) {
+        return ResponseDto.<CustomPage<GetRestaurantSimpleResponseDto>>builder()
+                .data(toCustomPageDto(entityPage))
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .build();
+    }
+
+    private CustomPage<GetRestaurantSimpleResponseDto> toCustomPageDto(Page<Restaurant> entityPage) {
+        return new CustomPage<>(toGetListDto(entityPage.getContent()),
+                new PageInfo(entityPage.getPageable().getOffset(), entityPage.getSize(),
+                        entityPage.getTotalElements(), entityPage.isFirst(),
+                        entityPage.getNumberOfElements(), entityPage.isFirst(),
+                        entityPage.getTotalPages()));
+
+    }
+
+    private List<GetRestaurantSimpleResponseDto> toGetListDto(List<Restaurant> entityList) {
+        return entityList.stream().map(this::toGetSimpleDto).toList();
+    }
+
+    private GetRestaurantSimpleResponseDto toGetSimpleDto(Restaurant entity) {
+        return GetRestaurantSimpleResponseDto.builder()
+                .restaurantId(entity.getId())
+                .restaurantName(entity.getName())
+                .lotNumberAddress(entity.getLotNumberAddress())
+                .roadNameAddress(entity.getRoadNameAddress())
+                .longitude(entity.getLongitude())
+                .latitude(entity.getLatitude())
+                .location(toDto(entity.getLocation()))
+                .averageScore(entity.getAverageScore())
                 .build();
     }
 
