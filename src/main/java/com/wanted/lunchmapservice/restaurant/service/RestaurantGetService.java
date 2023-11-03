e26a88ebdb446a91a7ea740c22734bbbaff94ca8
Siglee2247 <lgh941107@gmail.com>
2023-11-03 10:35:58 +0900

    fit: Restaurant getRestaurant 메서드 구현을 위한 service 구현 #1

--- src/main/java/com/wanted/lunchmapservice/restaurant/service/RestaurantGetService.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/service/RestaurantGetService.java
@@ -1,15 +1,20 @@
 package com.wanted.lunchmapservice.restaurant.service;
 
+import com.wanted.lunchmapservice.common.dto.CustomPage;
 import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.common.exception.CommonException;
 import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantSimpleResponseDto;
+import com.wanted.lunchmapservice.restaurant.dto.RestaurantGetFilterDto;
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
 import com.wanted.lunchmapservice.restaurant.mapper.RestaurantMapper;
 import com.wanted.lunchmapservice.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,9 +29,13 @@ public class RestaurantGetService {
         return mapper.toResponseDto(result);
     }
 
+    public ResponseDto<CustomPage<GetRestaurantSimpleResponseDto>> getRestaurant(@ModelAttribute RestaurantGetFilterDto request, Pageable pageable) {
+        return mapper.toResponseDto(repository.findPageByFilter(pageable,request));
+    }
 
     private Restaurant validRestaurant(Long restaurantId) {
         return repository.findById(restaurantId)
                 .orElseThrow(() -> new CommonException(HttpStatus.CONFLICT, "맛집 정보가 존재하지 않습니다."));
     }
+
 }
