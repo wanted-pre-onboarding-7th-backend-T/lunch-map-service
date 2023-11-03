96eef61300b25652c9b134e61954a3d3bfd21999
Siglee2247 <lgh941107@gmail.com>
2023-11-03 10:36:09 +0900

    fit: Restaurant getRestaurant 메서드 구현을 위한 controller 구현 #1

--- src/main/java/com/wanted/lunchmapservice/restaurant/controller/RestaurantController.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/controller/RestaurantController.java
@@ -1,11 +1,16 @@
 package com.wanted.lunchmapservice.restaurant.controller;
 
+import com.wanted.lunchmapservice.common.dto.CustomPage;
 import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.dto.GetRestaurantSimpleResponseDto;
+import com.wanted.lunchmapservice.restaurant.dto.RestaurantGetFilterDto;
 import com.wanted.lunchmapservice.restaurant.service.RestaurantGetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
 public class RestaurantController {
+
     private final RestaurantGetService getService;
 
     @GetMapping("/{restaurantId}")
-    public ResponseEntity<ResponseDto<GetRestaurantDetailResponseDto>> getRestaurantDetail(@PathVariable Long restaurantId) {
+    public ResponseEntity<ResponseDto<GetRestaurantDetailResponseDto>> getRestaurantDetail(
+            @PathVariable Long restaurantId) {
         return ResponseEntity.ok(getService.getDetails(restaurantId));
     }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto<CustomPage<GetRestaurantSimpleResponseDto>>> getRestaurant(
+            @ModelAttribute RestaurantGetFilterDto request, Pageable pageable) {
+        return ResponseEntity.ok(getService.getRestaurant(request, pageable));
+    }
+
 }
