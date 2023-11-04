d36a7fa9ff8b5d915794fe142bccb0522249300b
Daeun Kim <jkde7721@naver.com>
2023-11-04 20:07:55 +0900

    feat: Open API 호출하여 식당 데이터 동기화하는 스케줄러 서비스 구현 #2
    - Open API 호출 응답 데이터를 RawRestaurant 테이블에 동기화
    - RawRestaurant 테이블 데이터를 Restaurant 테이블에 동기화

---
+++ src/main/java/com/wanted/lunchmapservice/restaurant/utils/SchedulerService.java
@@ -0,0 +1,54 @@
+package com.wanted.lunchmapservice.restaurant.utils;
+
+import com.wanted.lunchmapservice.location.entity.Location;
+import com.wanted.lunchmapservice.location.utils.LocationCsvService;
+import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
+import com.wanted.lunchmapservice.restaurant.repository.RawRestaurantRepository;
+import com.wanted.lunchmapservice.restaurant.entity.RawRestaurant;
+import com.wanted.lunchmapservice.restaurant.repository.RestaurantRepository;
+import com.wanted.lunchmapservice.restaurant.utils.openapi.OpenApiCaller;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class SchedulerService {
+
+    private final OpenApiCaller apiCaller;
+    private final LocationCsvService locationCsvService;
+    private final RawRestaurantRepository rawRestaurantRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    @Transactional
+    @Scheduled(initialDelay = 30000, fixedDelay = 3600000) //서버 시작 30초 후, 이후에는 1시간 마다
+//    @Scheduled(cron = "59 59 23 ? * FRI") //매주 금요일 23시 59분 59초 마다
+    public void syncRestaurantData() {
+        rawRestaurantRepository.findAll(); //영속성 컨텍스트 영속화
+        List<RawRestaurant> rawRestaurantList = apiCaller.callApi();
+        rawRestaurantRepository.saveAll(rawRestaurantList); //update or insert
+        syncRestaurantData(rawRestaurantList);
+    }
+
+    private void syncRestaurantData(List<RawRestaurant> rawRestaurantList) {
+        for (RawRestaurant rawRestaurant : rawRestaurantList) {
+            restaurantRepository.findByNameAndLotNumberAddress(
+                    rawRestaurant.getName(), rawRestaurant.getLotNumberAddress())
+                .ifPresentOrElse(restaurant -> updateData(rawRestaurant, restaurant),
+                    () -> insertData(rawRestaurant));
+        }
+    }
+
+    private void updateData(RawRestaurant rawRestaurant, Restaurant restaurant) {
+        Location location = locationCsvService.getLocation(rawRestaurant.getLocationCode());
+        restaurant.update(location, rawRestaurant);
+    }
+
+    private void insertData(RawRestaurant rawRestaurant) {
+        Location location = locationCsvService.getLocation(rawRestaurant.getLocationCode());
+        Restaurant restaurant = Restaurant.of(location, rawRestaurant);
+        restaurantRepository.save(restaurant);
+    }
+}
