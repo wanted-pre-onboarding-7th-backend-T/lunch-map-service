9fbbac7ff982f3b8be4b20c3fbfe4a52a24593eb
Byeonghee <0722ths@gmail.com>
2023-11-05 10:57:26 +0900

    feat: 사용자 설정 업데이트, 사용자 정보 controller 구현

--- src/main/java/com/wanted/lunchmapservice/user/controller/UserController.java
+++ src/main/java/com/wanted/lunchmapservice/user/controller/UserController.java
@@ -2,13 +2,19 @@ package com.wanted.lunchmapservice.user.controller;
 
 import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.user.dto.request.UserPostRequestDto;
+import com.wanted.lunchmapservice.user.dto.request.UserUpdateRequestDto;
 import com.wanted.lunchmapservice.user.dto.response.UserIdResponseDto;
+import com.wanted.lunchmapservice.user.dto.response.UserInfoResponseDto;
 import com.wanted.lunchmapservice.user.service.UserService;
 import com.wanted.lunchmapservice.user.utils.UriCreator;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +36,18 @@ public class UserController {
         return ResponseEntity.created(location).body(result);
     }
 
+    @PatchMapping("/{userId}")
+    public ResponseEntity<ResponseDto<UserInfoResponseDto>> updateUser(
+        @PathVariable Long userId,
+        @RequestBody @Valid UserUpdateRequestDto updateDto) {
+        UserInfoResponseDto result = service.updateUser(userId, updateDto);
+        return ResponseEntity.ok(new ResponseDto<>(HttpStatus.OK.value(), result, HttpStatus.OK.getReasonPhrase()));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseDto<UserInfoResponseDto>> getUser(
+        @PathVariable Long userId) {
+        UserInfoResponseDto result = service.getUser(userId);
+        return ResponseEntity.ok(new ResponseDto<>(HttpStatus.OK.value(), result, HttpStatus.OK.getReasonPhrase()));
+    }
 }
