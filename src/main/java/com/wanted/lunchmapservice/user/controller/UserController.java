66225687456635cee61f1be0d37907bef8592e31
Byeonghee <0722ths@gmail.com>
2023-11-06 23:38:43 +0900

    feat: 사용자 설정 업데이트 controller 구현

--- src/main/java/com/wanted/lunchmapservice/user/controller/UserController.java
+++ src/main/java/com/wanted/lunchmapservice/user/controller/UserController.java
@@ -4,17 +4,13 @@ import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.user.dto.request.UserPostRequestDto;
 import com.wanted.lunchmapservice.user.dto.request.UserUpdateRequestDto;
 import com.wanted.lunchmapservice.user.dto.response.UserIdResponseDto;
-import com.wanted.lunchmapservice.user.dto.response.UserInfoResponseDto;
 import com.wanted.lunchmapservice.user.service.UserService;
 import com.wanted.lunchmapservice.user.utils.UriCreator;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,19 +31,15 @@ public class UserController {
         URI location = UriCreator.createUri(URL, result.getData().getUserId());
         return ResponseEntity.created(location).body(result);
     }
+    @PatchMapping("/info")
+    public ResponseEntity<?> updateUserInfo(@Valid @RequestBody UserUpdateRequestDto settingsDto) {
+        // You may want to include authentication and authorization checks
+        // to ensure that the user is permitted to update the information.
 
-    @PatchMapping("/{userId}")
-    public ResponseEntity<ResponseDto<UserInfoResponseDto>> updateUser(
-        @PathVariable Long userId,
-        @RequestBody @Valid UserUpdateRequestDto updateDto) {
-        UserInfoResponseDto result = service.updateUser(userId, updateDto);
-        return ResponseEntity.ok(new ResponseDto<>(HttpStatus.OK.value(), result, HttpStatus.OK.getReasonPhrase()));
-    }
+        // The service layer would handle the update logic
+        userService.updateUserSettings(settingsDto);
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<ResponseDto<UserInfoResponseDto>> getUser(
-        @PathVariable Long userId) {
-        UserInfoResponseDto result = service.getUser(userId);
-        return ResponseEntity.ok(new ResponseDto<>(HttpStatus.OK.value(), result, HttpStatus.OK.getReasonPhrase()));
+        // Return an appropriate response, such as:
+        return ResponseEntity.ok().build();
     }
 }
