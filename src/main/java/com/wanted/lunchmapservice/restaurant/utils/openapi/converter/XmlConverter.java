b0325e97432ae866314ff761813d6b46271493aa
Daeun Kim <jkde7721@naver.com>
2023-11-06 11:52:40 +0900

    test: XML 데이터 포맷터, 컨버터 테스트 #2

--- src/main/java/com/wanted/lunchmapservice/restaurant/utils/openapi/converter/XmlConverter.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/utils/openapi/converter/XmlConverter.java
@@ -1,10 +1,12 @@
 package com.wanted.lunchmapservice.restaurant.utils.openapi.converter;
 
+import com.wanted.lunchmapservice.common.exception.CommonException;
 import com.wanted.lunchmapservice.restaurant.utils.openapi.OpenApiDto;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
 import java.io.StringReader;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +18,7 @@ public class XmlConverter {
             Unmarshaller unmarshaller = context.createUnmarshaller();
             return (OpenApiDto) unmarshaller.unmarshal(new StringReader(xmlData));
         } catch (JAXBException e) {
-            throw new RuntimeException(); //TODO: 예외처리
+            throw new CommonException(HttpStatus.INTERNAL_SERVER_ERROR, "XML 형식이 올바르지 않습니다.");
         }
     }
 }
