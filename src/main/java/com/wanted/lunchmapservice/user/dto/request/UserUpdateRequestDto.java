0d8bca9e700dce8951ee37961461270d7c6bd0c4
Byeonghee <0722ths@gmail.com>
2023-11-06 23:35:09 +0900

    feat: 사용자 설정 업데이트 dto 구현

--- src/main/java/com/wanted/lunchmapservice/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/wanted/lunchmapservice/user/dto/request/UserUpdateRequestDto.java
@@ -2,13 +2,26 @@ package com.wanted.lunchmapservice.user.dto.request;
 
 import com.wanted.lunchmapservice.user.enums.ServiceAccess;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserUpdateRequestDto {
 
     private Double latitude;
     private Double longitude;
     private ServiceAccess serviceAccess;
+
+    @Getter
+    private Long userId;
+
+    public void setServiceAccess(ServiceAccess serviceAccess) {
+        this.serviceAccess = serviceAccess;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
